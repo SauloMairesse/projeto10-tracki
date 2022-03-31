0f424ed29086c3412aa61ec1a5664aa765c0ca4c
Saulo Davi <saulodavi2001@gmail.com>
2022-03-31 19:35:26 -0300

    Start Register Screen

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,9 @@
 
 
 export default function Register(){
-    
+
     return(
-        <LoginScreen>
+        <RegisterScreen>
             <img src="../images/Group8.png" alt="" />
             <form action="">
                 <input type="text"  value={nameUser} 
@@ -12,9 +12,14 @@ export default function Register(){
                 <input type="text"  value={passwordUser} 
                                     placeholder={'Senha'} 
                                     onChange={ (e) => setPasswordUser(e.target.value) }/>
-                <button> Entrar </button>
+                <input type="text"  value={nameUser} 
+                                    placeholder={'Email'} 
+                                    onChange={ (e) => setNameUser(e.target.value) }/>
+                <input type="text"  value={passwordUser} 
+                                    placeholder={'Senha'} 
+                                    onChange={ (e) => setPasswordUser(e.target.value) }/>
+                <button> Cadastrar </button>
             </form>
-            <Link to={`/cadastro`}> <span> Ainda não possui Cadastro ? Cadastre-se </span> </Link>
-        </LoginScreen>
+        </RegisterScreen>
     )
 }
